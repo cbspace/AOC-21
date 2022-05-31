8378f52cd39bcb830c1d34ec398d13c25eb12b93
cbspace <24204459+cbspace@users.noreply.github.com>
2022-05-31 17:36:23 +1000

    Day 10

--- Day10_2.py
+++ Day10_2.py
@@ -3,16 +3,12 @@ import sys
 # Input lines to be parsed
 line_array = []
 
-# Stack for parsing
-lstack = []
-
 # Valid characters
 open_brackets = ['(','[','{','<']
 closed_brackets = [')',']','}','>']
 
 # Points
-points_table = [3,57,1197,25137]
-total_points = 0
+points_table = []
 
 # Input file
 infile = open("Day10_input.txt", "r")
@@ -20,6 +16,8 @@ for l in infile:
 	line_array.append(l.strip())
 
 for line in line_array:
+	lstack = []
+	total_points = 0
 	for c in line:
 		if c in open_brackets:
 			lstack.append(c)
@@ -27,5 +25,11 @@ for line in line_array:
 			if open_brackets[closed_brackets.index(c)] != lstack.pop():
 				total_points += points_table[closed_brackets.index(c)]
 				continue
+	for s in reversed(lstack):
+		print(lstack.pop())
+		total_points *= 5
+		total_points += open_brackets.index(s)+1
+		line_scores.append(total_points)
 
-print(total_points)
+line_scores.sort()
+print(line_scores[len(line_scores/2)])
